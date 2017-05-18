1e62d055f42a89e7f8e5d01dabf0fc1b6f911847
Jayse Farrell <jafarrellpersonal@gmail.com>
2017-05-18 14:09:07 -0700

    level select

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public Button btn;
+	// Use this for initialization
+	void Start () {
+		btn.onClick.AddListener(() =>
+		{
+            Debug.Log("reached Here");
+		    SceneManager.LoadScene("Sample_Terrain");
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
